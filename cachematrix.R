17a980f25392e10a27d6469284dbd25780adaf82
Scott Riley <kscr70@gmail.com>
2015-01-21 21:07:43 -0800

    tested without the set function. Still worked. Put Set function back in. Added final comments.

--- cachematrix.R
+++ cachematrix.R
@@ -6,31 +6,32 @@
 ## that can be called on with the cacheSolve function.
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
-        set <- function(y) {
+        m <- NULL  # gives m a null value
+        set <- function(y) {    # allows the original matrix to be reset by calling the set function.
                 x <<- y
                 m <<- NULL
         }
         get <- function() x  # assigns matrix(x) to get
-        setInvMatrix <- function(solve) m <<- solve 
-        getInvMatrix <- function() m
-        list(set = set, get = get,
+        setInvMatrix <- function(solve) m <<- solve #sets the variable solve to m from the parent environment
+        getInvMatrix <- function() m   # r
+        list(set = set, get = get,     #prints the list of functions and their environments
              setInvMatrix = setInvMatrix,
              getInvMatrix = getInvMatrix)
         
 }
 
 
-## Write a short comment describing this function
+## this function calculates the inverse of the matrix from makeCacheMatrix function
+## and if it has already been caluculated it retrieves it from memory.
 
 cacheSolve <- function(x, ...) {
-        m <- x$getInvMatrix()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
+        m <- x$getInvMatrix() #assigns m the value of the inverse matrix.
+        if(!is.null(m)) {     
+                message("getting cached data")  #if the inverse has already been calculated (m not NULL) it gets m from memory
+                return(m)                       #and prints it
         }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$setInvMatrix(m)
-        m
+        data <- x$get()         #gets the matrix x
+        m <- solve(data, ...)   #sets m to the inverse of the matrix x
+        x$setInvMatrix(m)       #passes m to the setInvMatrix function
+        m                       #prints the inverse of matrix x
 }
